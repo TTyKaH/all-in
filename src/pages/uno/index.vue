8a8912f432dd8237f369c46d292e445d4d4f989f
vitaly <vitaly.grudachev@servermania.com>
2021-11-06 02:17:31 +0300

    upd

--- src/pages/uno/index.vue
+++ src/pages/uno/index.vue
@@ -3,10 +3,15 @@
     <div class="wrap wrap-pt">
       <h2 class="other-font">Uno</h2>
       <div class="settings">
-        <button class="btn">Set</button>
-        <input type="text" placeholder="points limit (500 st.)" />
-        <button class="btn">Add</button>
-        <input type="text" placeholder="player name" />
+        <button class="btn btn-small" @click="set()">Set</button>
+        <input
+          id="newLimitScore"
+          type="text"
+          placeholder="endgame score (500 st.)"
+          v-model="newLimitScore"
+        />
+        <button class="btn btn-small" @click="add()">Add</button>
+        <input type="text" placeholder="player name" v-model="newPlayer" />
       </div>
       <div class="line line-mt"></div>
     </div>
@@ -15,33 +20,33 @@
         <div class="table-head grid-24">
           <div class="span-9">Player Name</div>
           <div class="span-4">Score</div>
-          <div class="span-7">per Round</div>
+          <div class="span-7 self-center">per Round</div>
           <div class="span-4"></div>
         </div>
         <div class="table-body">
-          <div class="table-row grid-24">
-            <div class="span-9">Vitaly</div>
-            <div class="span-4">0</div>
-            <input type="text" class="span-7" />
-            <div class="span-4 btn btn-small">Add</div>
-          </div>
-          <div class="table-row grid-24">
-            <div class="span-9">Sergey</div>
-            <div class="span-4">24</div>
-            <input type="text" class="span-7" />
-            <div class="span-4 btn btn-small">Add</div>
-          </div>
-          <div class="table-row grid-24">
-            <div class="span-9">Michail</div>
-            <div class="span-4">300</div>
-            <input type="text" class="span-7" />
-            <div class="span-4 btn btn-small">Add</div>
+          <div
+            v-for="(player, idx) in players"
+            :key="idx"
+            class="table-row grid-24"
+          >
+            <div class="span-9">{{ player.name }}</div>
+            <div class="span-4">{{ player.score }}</div>
+            <input
+              type="text"
+              class="span-7"
+              v-model="players[idx].scorePerRound"
+            />
+            <div class="span-4 btn btn-small" @click="plus(idx)">+</div>
           </div>
         </div>
       </div>
+      <div class="endgameScore">
+        <div>Endgame Score:</div>
+        <div>{{ limitScore }}</div>
+      </div>
       <div class="line line-mt"></div>
     </div>
-    <div class="wrap wrap-pt">
+    <div class="wrap wrap-py">
       <div class="chart">
         <div class="horizontal-line"></div>
         <div class="column">
@@ -54,7 +59,60 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      newLimitScore: "",
+      limitScore: 500,
+      newPlayer: "",
+      players: [
+        {
+          name: "Vitaly",
+          score: 100,
+          scorePerRound: 0,
+          roundsResults: [],
+        },
+        {
+          name: "Sergey",
+          score: 100,
+          scorePerRound: 0,
+          roundsResults: [],
+        },
+        {
+          name: "Michail",
+          score: 100,
+          scorePerRound: 0,
+          roundsResults: [],
+        },
+      ],
+    };
+  },
+  methods: {
+    set() {
+      // Сделать всплывающее окно с небольшими пояснениями по ограничениям и правилам
+      let element = document.getElementById("newLimitScore");
+      if (this.newLimitScore >= 300 && this.newLimitScore < 1000) {
+        element.classList.remove("error");
+        return (this.limitScore = this.newLimitScore);
+      }
+      return element.classList.add("error");
+    },
+    add() {
+      this.players.push({
+        name: this.newPlayer,
+        score: 0,
+        scorePerRound: 0,
+        // возможно нужен счетчик раундов, чтобы кто-то контролировал раунды (это на
+        // случай, если за один раунд игроку будет проводится несколько операций суммирования)
+        roundsResults: [],
+      });
+    },
+    plus(idx) {
+      this.players[idx].score =
+        this.players[idx].score + Number(this.players[idx].scorePerRound);
+    },
+  },
+};
 </script>
 
 <style lang="scss">
@@ -68,6 +126,12 @@ export default {};
     display: grid;
     grid-template: 1fr 1fr / 1fr 3fr;
     gap: 15px 10px;
+  }
+
+  .endgameScore {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 15px;
   }
 }
 </style>
