741b68a440f2fbaa9d529b6a78d7473d487c8b5e
vitaly <vitaly.grudachev@servermania.com>
2021-11-07 19:26:24 +0300

    upd

--- src/pages/uno/index.vue
+++ src/pages/uno/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="uno">
-    <section>
+    <section id="settings-field">
       <div class="wrap wrap-pt">
         <h2 class="font-fruktur">Uno</h2>
         <div class="settings">
@@ -22,83 +22,101 @@
         <div class="line line-mt"></div>
       </div>
     </section>
-    <div v-if="players" class="wrap wrap-pt">
-      <div class="table">
-        <div class="table-head grid-24">
-          <div class="span-9">Player Name</div>
-          <div class="span-4">Score</div>
-          <div class="span-7 self-center">per Round</div>
-          <div class="span-4"></div>
+    <section id="action-field">
+      <div class="wrap wrap-pt">
+        <div v-if="!players.length" class="note">
+          Add players to get started!
         </div>
-        <div class="table-body">
-          <div
-            v-for="(player, idx) in players"
-            :key="idx"
-            class="table-row grid-24"
-          >
-            <div class="span-9" :class="{ loser: player.score >= 500 }">
-              {{ player.name }}
+        <div v-else>
+          <div class="table">
+            <div class="table-head grid-24">
+              <div class="span-9">Player Name</div>
+              <div class="span-4">Score</div>
+              <div class="span-7 self-center">per Round</div>
+              <div class="span-4"></div>
             </div>
-            <div class="span-4">{{ player.score }}</div>
-            <input
-              type="text"
-              class="span-7"
-              v-model="players[idx].scorePerRound"
-            />
-            <div class="span-4 btn btn-small" @click="sum(idx)">+</div>
+            <div class="table-body">
+              <div
+                v-for="(player, idx) in players"
+                :key="idx"
+                class="table-row grid-24"
+              >
+                <div class="span-9" :class="{ loser: player.score >= 500 }">
+                  {{ player.name }}
+                </div>
+                <div class="span-4">{{ player.score }}</div>
+                <input
+                  type="text"
+                  class="span-7"
+                  v-model="players[idx].scorePerRound"
+                />
+                <div class="span-4 btn btn-small" @click="sum(idx)">+</div>
+              </div>
+            </div>
+          </div>
+          <div class="sum-all grid grid-24">
+            <div class="span-20"></div>
+            <div class="span-4 btn btn-small" @click="sumAll()">Σ+</div>
+          </div>
+          <div class="endgameScore">
+            <div>Endgame Score:</div>
+            <div>{{ limitScore }}</div>
+          </div>
+          <div class="roundsCount grid grid-12">
+            <button class="span-5 btn btn-small" @click="prev()">
+              prev round
+            </button>
+            <div class="span-2">{{ round }}</div>
+            <button class="span-5 btn btn-small" @click="next()">
+              next round
+            </button>
           </div>
         </div>
       </div>
-      <div class="sum-all grid grid-24">
-        <div class="span-20"></div>
-        <div class="span-4 btn btn-small" @click="sumAll()">Σ+</div>
-      </div>
-      <div class="endgameScore">
-        <div>Endgame Score:</div>
-        <div>{{ limitScore }}</div>
-      </div>
-      <div class="roundsCount grid grid-12">
-        <button class="span-5 btn btn-small" @click="prev()">prev round</button>
-        <div class="span-2">{{ round }}</div>
-        <button class="span-5 btn btn-small" @click="next()">next round</button>
-      </div>
-    </div>
-    <div class="wrap wrap-pb">
-      <div class="line line-my"></div>
-      <div class="chart">
-        <p>bar graph</p>
-        <div class="axis">
-          <div class="axis-points grid grid-24">
-            <div class="span-4">0</div>
-            <div class="span-4 self-center">
-              {{ Math.floor(limitScore / 4) }}
+    </section>
+    <section id="statistic">
+      <div class="wrap wrap-pb">
+        <div class="line line-my"></div>
+        <div class="chart">
+          <p>bar graph</p>
+          <div class="axis">
+            <div class="axis-points grid grid-24">
+              <div class="span-4">0</div>
+              <div class="span-4 self-center">
+                {{ Math.floor(limitScore * 0.25) }}
+              </div>
+              <div class="span-8 self-center">
+                {{ Math.floor(limitScore * 0.5) }}
+              </div>
+              <div class="span-4 self-center">
+                {{ Math.floor(limitScore * 0.75) }}
+              </div>
+              <div class="span-4 self-end">{{ limitScore }}</div>
             </div>
-            <div class="span-8 self-center">
-              {{ Math.floor(limitScore / 2) }}
+            <div class="axis-line grid grid-24">
+              <div class="span-6"></div>
+              <div class="span-6"></div>
+              <div class="span-6"></div>
+              <div class="span-6"></div>
             </div>
-            <div class="span-4 self-center">
-              {{ Math.floor(limitScore * 0.75) }}
-            </div>
-            <div class="span-4 self-end">{{ limitScore }}</div>
           </div>
-          <div class="axis-line grid grid-24">
-            <div class="span-6"></div>
-            <div class="span-6"></div>
-            <div class="span-6"></div>
-            <div class="span-6"></div>
-          </div>
-        </div>
-        <div class="lines">
-          <div v-for="(player, idx) in players" :key="idx" class="score">
-            <div class="player-name">
-              <span>{{ player.name }}:</span>
-              <span>{{ player.score }}</span>
+          <div class="lines">
+            <div v-for="(player, idx) in players" :key="idx" class="score">
+              <div class="player-name">
+                <span>{{ player.name }}:</span>
+                <span>{{ player.score }}</span>
+              </div>
+              <div
+                class="score-line"
+                :class="{ 'loser-bg': player.score > limitScore }"
+                :style="{ width: culcWidth(idx) }"
+              />
             </div>
-            <div class="score-line" :style="{ width: culcWidth(idx) }" />
           </div>
         </div>
+        <button class="btn btn-small" @click="clear()">Clear Game</button>
       </div>
-    </div>
+    </section>
   </div>
 </template>
 
@@ -110,36 +128,36 @@ export default {
       limitScore: 500,
       newPlayer: "",
       players: [
-        // {
-        //   name: "Vitaly",
-        //   score: 450,
-        //   scorePerRound: "",
-        //   roundsResults: [60, 34, 124],
-        // },
-        // {
-        //   name: "Sergey",
-        //   score: 375,
-        //   scorePerRound: "",
-        //   roundsResults: [],
-        // },
-        // {
-        //   name: "Michail",
-        //   score: 250,
-        //   scorePerRound: "",
-        //   roundsResults: [],
-        // },
-        // {
-        //   name: "David",
-        //   score: 125,
-        //   scorePerRound: "",
-        //   roundsResults: [],
-        // },
-        // {
-        //   name: "Lera",
-        //   score: 150,
-        //   scorePerRound: "",
-        //   roundsResults: [],
-        // },
+        {
+          name: "Vitaly",
+          score: 550,
+          scorePerRound: "",
+          roundsResults: [60, 34, 124],
+        },
+        {
+          name: "Sergey",
+          score: 375,
+          scorePerRound: "",
+          roundsResults: [],
+        },
+        {
+          name: "Michail",
+          score: 250,
+          scorePerRound: "",
+          roundsResults: [],
+        },
+        {
+          name: "David",
+          score: 125,
+          scorePerRound: "",
+          roundsResults: [],
+        },
+        {
+          name: "Lera",
+          score: 150,
+          scorePerRound: "",
+          roundsResults: [],
+        },
       ],
       round: 1,
     };
@@ -198,6 +216,9 @@ export default {
       }
       return (this.players[idx].score / this.limitScore) * 100 + "%";
     },
+    clear() {
+      this.players = [];
+    },
   },
 };
 </script>
@@ -209,75 +230,95 @@ export default {
     margin-bottom: 10px;
   }
 
-  .settings {
-    display: grid;
-    grid-template: 1fr 1fr / 1fr 3fr;
-    gap: 15px 10px;
-  }
-
-  .sum-all {
-    margin-top: 10px;
-    gap: 10px;
-  }
-
-  .endgameScore {
-    display: flex;
-    justify-content: space-between;
-    margin: 15px 0;
-  }
-
-  .roundsCount {
-    gap: 10px;
-
-    > div {
-      text-align: center;
-      border-radius: 3px;
+  #settings-field {
+    .settings {
+      display: grid;
+      grid-template: 1fr 1fr / 1fr 3fr;
+      gap: 15px 10px;
     }
   }
 
-  .chart {
-    p {
-      text-transform: uppercase;
+  #action-field {
+    .note {
       text-align: center;
-      margin-bottom: 15px;
     }
 
-    .axis {
-      .axis-line {
-        > div {
-          border: #fff solid 2px;
-          border-top: 0;
-          border-left: 0;
-          width: 100%;
-          height: 5px;
+    .sum-all {
+      margin-top: 10px;
+      gap: 10px;
+    }
 
-          &:first-child {
+    .endgameScore {
+      display: flex;
+      justify-content: space-between;
+      margin: 15px 0;
+    }
+
+    .roundsCount {
+      gap: 10px;
+
+      > div {
+        text-align: center;
+        border-radius: 3px;
+      }
+    }
+  }
+
+  #statistic {
+    .wrap {
+      display: grid;
+    }
+
+    .chart {
+      p {
+        text-transform: uppercase;
+        text-align: center;
+        margin-bottom: 15px;
+      }
+
+      .axis {
+        .axis-line {
+          > div {
             border: #fff solid 2px;
             border-top: 0;
+            border-left: 0;
+            width: 100%;
+            height: 5px;
+
+            &:first-child {
+              border: #fff solid 2px;
+              border-top: 0;
+            }
+          }
+        }
+      }
+
+      .lines {
+        display: grid;
+        grid-template-rows: auto;
+        gap: 10px;
+        margin: 15px 0;
+
+        .score {
+          width: 100%;
+
+          .player-name {
+            display: flex;
+            gap: 10px;
+          }
+
+          .score-line {
+            height: 20px;
+            background-color: #408ad2;
           }
         }
       }
     }
 
-    .lines {
-      display: grid;
-      grid-template-rows: auto;
-      gap: 10px;
-      margin: 15px 0;
-
-      .score {
-        width: 100%;
-
-        .player-name {
-          display: flex;
-          gap: 10px;
-        }
-
-        .score-line {
-          height: 20px;
-          background-color: #408ad2;
-        }
-      }
+    .btn {
+      padding-left: 15px;
+      padding-right: 15px;
+      margin: 10px auto;
     }
   }
 }
